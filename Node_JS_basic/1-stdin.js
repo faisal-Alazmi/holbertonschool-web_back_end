b9fcf3561e1fa59b88470eff51894ff75a592ed4
faisal-Alazmi <fa41sa@hotmail.com>
2026-04-11 22:54:56 +0300

    Remove exit on data event in stdin.js

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,11 +1,7 @@
-// 1-stdin.js
-
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (data) => {
   process.stdout.write(`Your name is: ${data}`);
-  process.stdout.write('This important software is now closing\n');
-  process.exit();
 });
 
 process.stdin.on('end', () => {
