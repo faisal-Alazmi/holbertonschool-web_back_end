7d0377012aebc10cb8477244b5407bd03f42fe12
faisal-Alazmi <fa41sa@hotmail.com>
2026-04-11 23:02:32 +0300

    Implement HTTP server to handle student requests

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,50 @@
+const http = require('http');
+const fs = require('fs');
+
+const countStudents = (path) => new Promise((resolve, reject) => {
+  fs.readFile(path, 'utf8', (err, data) => {
+    if (err) {
+      reject(new Error('Cannot load the database'));
+      return;
+    }
+
+    const lines = data.split('\n').filter((line) => line.trim() !== '');
+    const students = lines.slice(1);
+
+    let output = `Number of students: ${students.length}\n`;
+
+    const fields = {};
+    students.forEach((line) => {
+      const [firstname, , , field] = line.split(',');
+      if (!fields[field]) fields[field] = [];
+      fields[field].push(firstname);
+    });
+
+    for (const [field, names] of Object.entries(fields)) {
+      output += `Number of students in ${field}: ${names.length}. List: ${names.join(', ')}\n`;
+    }
+
+    resolve(output.trim());
+  });
+});
+
+const app = http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+
+  if (req.url === '/') {
+    res.end('Hello Holberton School!');
+  } else if (req.url === '/students') {
+    const dbPath = process.argv[2];
+    countStudents(dbPath)
+      .then((result) => {
+        res.end(`This is the list of our students\n${result}`);
+      })
+      .catch((error) => {
+        res.end(`This is the list of our students\n${error.message}`);
+      });
+  }
+});
+
+app.listen(1245);
+
+module.exports = app;
